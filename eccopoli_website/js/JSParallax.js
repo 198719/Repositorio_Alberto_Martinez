144ac909f45a90659729ca3984797f4075fad479
Alberto Martínez <artedigital.martinez@gmail.com>
2019-12-11 23:11:27 -0600

    Commit 11-12-19

--- eccopoli_website/js/JSParallax.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function(){
-    var scrollval = $(this).scrollTop();    // It will return scroll value
-    
-    $(".parallax_lento").css("transform",'translate(0px,-'+scrollval/12+'%)');
-    $(".parallax_rapido").css("transform",'translate(0px,-'+scrollval/4+'%)');
-    
-});
